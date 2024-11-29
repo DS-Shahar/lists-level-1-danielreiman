6c2998f7c61696b69a787365cf9079d2b61627c0
Daniel Reiman <100288199+danielreiman@users.noreply.github.com>
2024-11-29 18:14:24 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,37 +1,112 @@
-class Main {
-  public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Node<Integer> n1 = new Node<>(7);
-		Node<Integer> n2 = new Node<>(2);
-		Node<Integer> n3 = new Node<>(8);
+import java.util.*;
+public class Main {
+	public static Scanner reader = new Scanner(System.in);
 
-		n1.setNext(n2);
-		n2.setNext(n3);
+	public static void main(String[] args) {
+		int[] array = {10, 20, 30, 40};
 		
-		System.out.println(n1);
-		
-		delNode(n1, 2);
-		
-		System.out.println(n1);
-
+		Node<Integer> head = buildList(array);
+	   	System.out.println(head.toString());
+	    
+	    ex2(head);
+	    
+	    Node<Integer> newHead = ex3();
+	    System.out.println(newHead);
+	    
+	    ex4(head);
+	    
+	    System.out.println(ex5(head, 20));
+	    
+	    head = ex6(head, 20);
+	    System.out.println(head.getNext());
+	
 	}
-public static Node<Integer> delNode(Node<Integer> h, int x) {
-		Node<Integer> p = new Node<>(-1, h);
+	
+	public static Node<Integer> buildList(int[] a) {
+		Node<Integer> h = new Node<Integer>(-1);
+		Node<Integer> p = h;
+		Node<Integer> x;
 		
-		while(p.hasNext()) {
-			
-			Node<Integer> t;
-			
-			if (p.getNext().getValue() == x) {
-				t = p.getNext();
-				p.setNext(p.getNext().getNext());
-				t.setNext(null);
-				return h.getNext();
-			}
-			
+		for (int i = 0; i < a.length; i++) {
+			x = new Node<Integer>(a[i]);
+			p.setNext(x);
 			p = p.getNext();
 		}
 		
-		return p.getNext();
+		return h.getNext();
+	}
+	
+	public static void ex2(Node<Integer> head) {
+		
+		Node<Integer> p = head;
+		
+		while (p.hasNext()) {
+			System.out.println(p.getValue());
+			p = p.getNext();
+		}
+	}
+	
+	public static Node<Integer> ex3() {
+	    Node<Integer> dhead = new Node<>(-1);
+	    Node<Integer> p = dhead;
+	    int input = reader.nextInt();
+	    while (input != -1) {
+		p.setNext(new Node<>(input));
+		p = p.getNext();
+		input = reader.nextInt();
+		
+	    }
+	    return dhead.getNext();
+	}
+	
+	public static void ex4(Node<Integer> head) {
+		Node<Integer> c = head;
+	    while (c.getNext() != null) {
+		if (c.getValue() % 2 == 0) {
+			System.out.println(c.getValue());
+		}
+		
+		c = c.getNext();
+	    }
+	}
+	
+	public static boolean ex5(Node<Integer> h, int t) {
+	    if (h.getValue() == t) {
+		return true;
+	    }
+	    
+	    return ex5(h.getNext(), t);
+	}
+	
+	public static Node<Integer> ex6(Node<Integer> h, int t) {
+		h = new Node<Integer>(-1, h);
+		Node<Integer> p = h;
+		boolean done = false;
+		while (!done && p.hasNext()) {
+			if (p.getNext().getValue() == t) {
+				p.setNext(p.getNext().getNext());
+				done = true;
+			} else {
+				p = p.getNext();
+			}
+		}
+		return h.getNext();
+	}
+	
+	public static Node<Integer> ex7(Node<Integer> head, int x) {
+		Node<Integer> c = head;
+		int count = 0;
+		
+		while (c.hasNext()) {
+			if (count == x) {
+				ex6(head, c.getValue());
+				return head;
+			}
+			
+			count++;
+			c = c.getNext();
+		}
+		
+		return head;
 	}
 }
